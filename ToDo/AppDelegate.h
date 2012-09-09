2c5497e72190d3a0d66108498f8a798147d6804e
GregoireJacquin <gregoire.jacquin@me.com>
2012-09-09 13:54:56 +0200

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Grégoire Jacquin on 09/09/12.
+//  Copyright (c) 2012 Grégoire Jacquin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
